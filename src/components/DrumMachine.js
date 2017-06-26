e350202616c71b884c04eca8db59d633a8c1a21b
csiejoey <asdfasdfasdf12tw@gmail.com>
2017-06-26 16:21:27 +0800

    add randomClicked btn

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -7,6 +7,7 @@ import key from 'keymaster';
 import NavigationMenuIcon from 'material-ui/svg-icons/navigation/menu';
 import NavigationRefreshIcon from 'material-ui/svg-icons/navigation/refresh';
 import NavigationCloseIcon from 'material-ui/svg-icons/navigation/close';
+import AVShuffleIcon from 'material-ui/svg-icons/av/shuffle';
 
 import styles from '../assets/styles/DrumMachine.css';
 import Matrix from './Matrix';
@@ -209,7 +210,24 @@ class DrumMachine extends Component {
 			data,
 		});
 	}
-
+  /**
+   * [clearClicked description]
+   * @param  {number} i first index
+   * @param  {number} j second index
+   */
+	randomClicked() {
+		const data = this.state.data;
+		let i;
+		let j;
+		for (i = 0; i < 16; i += 1) {
+			for (j = 0; j < 8; j += 1) {
+				data[i][j] = (Math.random() > 0.8) ? 1 : 0;
+			}
+		}
+		this.setState({
+			data,
+		});
+	}
   /**
    * [handleClick description]
    * @param  {number} i first index
@@ -668,7 +686,8 @@ class DrumMachine extends Component {
       <div className={(wait === true) ? styles.hideDOM : styles.showDOM}>
         <NavigationMenuIcon
           className={
-						`${styles.menuIcon}
+						`${styles.icon}
+						 ${styles.menuIcon}
 						 ${(hidden === true) ? '' : styles.displayHide}`
 					}
           onClick={() => this.toggleHidden()}
@@ -676,12 +695,23 @@ class DrumMachine extends Component {
         />
         <NavigationRefreshIcon
           className={
-						`${styles.clearIcon}
+						`${styles.icon}
+						 ${styles.clearIcon}
 						 ${(hidden === true) ? '' : styles.displayHide}`
 					}
           onClick={() => this.clearClicked()}
           style={{ color: '#eecdcc' }}
         />
+        <AVShuffleIcon
+          className={
+						`${styles.icon}
+						 ${styles.randomIcon}
+						 ${(hidden === true) ? '' : styles.displayHide}`
+					}
+          onClick={() => this.randomClicked()}
+          style={{ color: '#eecdcc' }}
+        />
+
         <div
           className={
 					`${styles.menu}
