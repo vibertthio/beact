68b98eeadf6b68bd135efbb6871ddfc2523016ef
Vibert Thio <vibertthio@gmail.com>
2017-06-26 10:28:27 +0800

    finish animation#19 moon

--- src/utils/Animation.js
+++ src/utils/Animation.js
@@ -1594,6 +1594,122 @@ function Animation() {
     return EXPORT;
   }());
 
+  /**
+   * Animation #19, Moon
+   * @param  {number} [opacity = 1]
+   * @param  {number} [duration = 400]
+   * @return {Object}
+   */
+  (function makeMoon(opacity = 1, duration = 500) {
+    let playing = false;
+    const amount = 42;
+    const half = amount * 0.5;
+    let radius = min(two.width, two.height) * 0.33;
+    const param = { ending: 0, beginning: 0 };
+
+    /**
+     * [setup description]
+     * @return {[type]} [description]
+     */
+    function setup() {
+      // first hemisphere
+      const moon = two.makePolygon(
+        two.width * 0.5,
+        two.height * 0.5,
+        radius,
+        amount,
+      );
+      moon.fill = pallete[4];
+      moon.noStroke();
+
+      const points = moon.vertices;
+      const destinations = [];
+      points.forEach((p, i) => {
+        const pct = i / (amount - 1);
+        const theta = pct * TWO_PI;
+        const x = radius * Math.cos(theta);
+        const y = radius * Math.sin(theta);
+        p.set(x, Math.abs(y));
+        destinations.push(new Two.Anchor(x, y));
+      });
+
+      const aniOut = new TWEEN.Tween(param)
+        .to({ beginning: 0 }, duration)
+        .easing(TWEEN.Easing.Sinusoidal.Out)
+        .onUpdate((t) => {
+          for (let i = 0; i < half; i += 1) {
+            points[i].y = lerp(points[i].y, -(destinations[i].y), t);
+          }
+        });
+
+      const aniIn = new TWEEN.Tween(param)
+        .to({ ending: 1.0 }, duration)
+        .easing(TWEEN.Easing.Sinusoidal.In)
+        .onUpdate((t) => {
+          for (let i = half; i < amount; i += 1) {
+            points[i].y = lerp(points[i].y, destinations[i].y, t);
+          }
+        })
+        .onComplete(() => {
+          aniOut.start();
+        });
+
+      return {
+        moon,
+        points,
+        destinations,
+        aniIn,
+        aniOut,
+      };
+    }
+
+    let { moon, points, destinations, aniIn, aniOut } = setup();
+
+    // methods
+    const resize = () => {
+      two.remove(moon);
+      radius = min(two.width, two.height) * 0.33;
+      ({ moon, points, destinations, aniIn, aniOut } = setup());
+    };
+
+    const reset = () => {
+      playing = false;
+      moon.visible = false;
+      moon.rotation = Math.random() * TWO_PI;
+      param.beginning = 0;
+      param.ending = 0;
+      for (let i = 0; i < amount; i += 1) {
+        const v = points[i];
+        const pct = i / (amount - 1);
+        const theta = pct * TWO_PI;
+        const x = radius * Math.cos(theta);
+        const y = radius * Math.sin(theta);
+        destinations[i].x = x;
+        destinations[i].y = y;
+        v.set(x, Math.abs(y));
+      }
+
+      aniIn.stop();
+      aniOut.stop();
+    };
+
+    const start = () => {
+      reset();
+      playing = true;
+      moon.visible = true;
+      aniIn.start();
+    };
+
+    const EXPORT = {
+      playing,
+      start,
+      reset,
+      resize,
+    };
+    animations.push(EXPORT);
+    return EXPORT;
+  }());
+
 
   const trigger = (index) => {
     animations[index].start();
