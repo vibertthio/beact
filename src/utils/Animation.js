db772f86337c1dbe64d4c890d4599deecad8a72a
Vibert Thio <vibertthio@gmail.com>
2017-06-27 17:58:13 +0800

    optimize animation#16 glimmer

--- src/utils/Animation.js
+++ src/utils/Animation.js
@@ -1294,32 +1294,31 @@ function Animation() {
    * random size, x, y, color circles.
    */
 	(function makeGlimmer(opacity = 1, duration = 400) {
+    let playing = false;
     /**
      * [setup description]
      * @return {[type]} [description]
      */
     function setup() {
-      const playing = false;
 			const amount = 12;
-      // const r1 = (two.height * 20) / 900;
-      // const r2 = (two.height * 40) / 900;
-
 			let longest = 0;
+
+      const anis = [];
 
 			const circles = _.map(_.range(amount), () => {
 				// const r = Math.round(map(Math.random(), 0, 1, r1, r2));
-				const r = Math.floor(Math.random() * 30);
+				const r = Math.floor(
+          min(two.width, two.height) *
+          ((1 + (Math.random() * 0.5)) / 40));
         const delay = Math.random() * duration * 0.5;
 				const circle = two.makeCircle(two.width * Math.random(), two.height * Math.random(), r);
         circle.noFill();
         circle.linewidth = 0;
-        circle.ani = new TWEEN.Tween(circle)
+        anis.push(new TWEEN.Tween(circle)
           .to({ scale: 1, linewidth: 0 }, duration * 1.5)
           .easing(TWEEN.Easing.Sinusoidal.Out)
-          .delay(delay)
-          .onComplete(() => {
-            circle.visible = false;
-          });
+          .delay(delay),
+        );
 
         if (longest < delay) {
           longest = delay;
@@ -1327,43 +1326,45 @@ function Animation() {
 
         return circle;
 			});
+      const ani = {
+        start: () => {
+          anis.forEach(a => a.start());
+        },
+        stop: () => {
+          anis.forEach(a => a.stop());
+        },
+      };
 
       return {
-        playing,
         circles,
+        ani,
       };
     }
 
-    let { playing, circles } = setup();
+    let { circles, ani } = setup();
 
     // methods
     const resize = () => {
       two.remove(circles);
-      ({ playing, circles } = setup());
+      ({ circles, ani } = setup());
     };
 
-		// let j;
     const reset = () => {
       playing = false;
-			// for (j = 0; j < circles.length; j++) {
-			// 	circles[j].visible = false;
-			// }
-      circles.opacity = 0;
+      ani.stop();
+      for (let i = 0; i < circles.length; i += 1) {
+        const c = circles[i];
+        c.translation.set(two.width * Math.random(), two.height * Math.random());
+        c.stroke = colors[i % colors.length];
+        c.scale = (Math.random() * 0.5) + 0.2;
+        c.linewidth = ((Math.random() + 0.5) + 1) * 8;
+      }
     };
 
-    let i;
-    let c;
     const start = () => {
       reset();
       playing = true;
-      for (i = 0; i < circles.length; i += 1) {
-        c = circles[i];
-				c.translation.set(two.width * Math.random(), two.height * Math.random());
-        c.visible = true;
-				c.stroke = colors[i % colors.length];
-				c.linewidth = 5;
-        c.ani.start();
-      }
+      ani.start();
     };
 
     const EXPORT = {
