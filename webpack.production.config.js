b301facf2e6111b50e8130299c60d54a5dacfaa0
Vibert Thio <vibertthio@gmail.com>
2017-07-11 03:47:47 +0800

    :guitar: remove hash from bundle in production build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -19,7 +19,7 @@ module.exports = {
   output: {
     publicPath: '/',
     path: path.join(__dirname, 'public'),
-    filename: '[chunkhash].bundle.js',
+    filename: 'bundle.js',
   },
   resolve: {
     extensions: ['.js', '.jsx']
