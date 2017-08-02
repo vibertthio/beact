fcad909a5a96b2c4c1ae791e2cbb91218c0a6f5b
Vibert Thio <vibertthio@gmail.com>
2017-08-02 16:37:42 +0800

    :fire: remove redundant comment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import MuiThemeProvider from '../../node_modules/material-ui/styles/MuiThemeProvider';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import DrumMachine from './DrumMachine';
 
